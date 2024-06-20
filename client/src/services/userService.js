a34175fe0c6e7b7b7f9913f1e58313a218463862
antoan-97 <antoan.todorov@abv.bg>
2024-06-20 15:23:11 +0300

    add create service

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,16 +1,48 @@
 const baseUrl = 'http://localhost:3030/jsonstore/users'
 
-export const getAll = async () =>{
+export const getAll = async () => {
     try {
 
         const response = await fetch(baseUrl);
-        const result =  await response.json();
-    
+        const result = await response.json();
+
         const data = Object.values(result);
-    
+
         return data
-        
+
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
-}
+};
+
+
+export const create = async (data) => {
+    const body = {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        imageUrl: data.imageUrl,
+        phoneNumber: data.phoneNumber,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+        adress: {
+            country: data.country,
+            city: data.city,
+            street: data.street,
+            streetNumber: data.streetNumber,
+        }
+    };
+
+    const resposne = await fetch(baseUrl,{
+        method:'POST',
+        headers:{
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    })
+
+    const result = await resposne.json()
+    console.log(result);
+    return result
+
+};
