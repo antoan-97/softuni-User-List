a9ff802e19c9b501cac0fc6cfc5e9af925b0de2f
antoan-97 <antoan.todorov@abv.bg>
2024-06-20 14:35:19 +0300

    hide modal on click

--- client/src/components/CreateModal.jsx
+++ client/src/components/CreateModal.jsx
@@ -1,12 +1,14 @@
-export default function CreateModal() {
+export default function CreateModal({
+    hideModal
+}) {
     return (
         <div class="overlay">
-            <div class="backdrop"></div>
+            <div class="backdrop" onClick={hideModal}></div>
             <div class="modal">
                 <div class="user-container">
                     <header class="headers">
                         <h2>Edit User/Add User</h2>
-                        <button class="btn close">
+                        <button class="btn close" onClick={hideModal}>
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark"
                                 class="svg-inline--fa fa-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                                 <path fill="currentColor"
@@ -93,7 +95,7 @@ export default function CreateModal() {
                         </div>
                         <div id="form-actions">
                             <button id="action-save" class="btn" type="submit">Save</button>
-                            <button id="action-cancel" class="btn" type="button">
+                            <button id="action-cancel" class="btn" type="button" onClick={hideModal}>
                                 Cancel
                             </button>
                         </div>
