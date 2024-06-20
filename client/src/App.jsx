76d5242b7f21599efe5d7983f7c5108e342daf9f
antoan-97 <antoan.todorov@abv.bg>
2024-06-20 12:38:33 +0300

    import Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer'
 import Header from './components/Header'
 import './styles.css'
 
@@ -8,6 +9,8 @@ function App() {
 
     <div>
       <Header />
+
+      <Footer />
     </div>
   )
 }
